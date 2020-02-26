f3ff0e4f3c87084bfcb38c05e284d5007489887f
tejuamirthi <tejuamirthi@gmail.com>
2020-02-26 22:56:37 +0530

    Service files created

---
+++ src/main/java/com/assignment/movielist/Services/MovieListService.java
@@ -0,0 +1,5 @@
+package com.assignment.movielist.Services;
+
+public interface MovieListService {
+    String getListMovies();
+}
---
+++ src/main/java/com/assignment/movielist/Services/MovieListServiceImplementation.java
@@ -0,0 +1,8 @@
+package com.assignment.movielist.Services;
+
+public class MovieListServiceImplementation implements MovieListService {
+    @Override
+    public String getListMovies() {
+        return null;
+    }
+}
