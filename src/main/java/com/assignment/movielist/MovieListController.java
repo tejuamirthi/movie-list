c6f9e1ec83626c9e8e843df3702b7386ccb088cb
tejuamirthi <tejuamirthi@gmail.com>
2020-02-26 22:53:17 +0530

    Parsing Director Name

--- src/main/java/com/assignment/movielist/MovieListController.java
+++ src/main/java/com/assignment/movielist/MovieListController.java
@@ -2,6 +2,7 @@ package com.assignment.movielist;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,15 +16,24 @@ public class MovieListController {
 
 
     @GetMapping(path = "/get-list")
-    public Document getMovieList() throws Throwable {
+    public String getMovieList() throws Throwable {
         String url = "https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page="+ URLEncoder.encode("The Matrix","UTF-8");
         RestTemplate restTemplate = new RestTemplate();
         ResponseParse response = restTemplate.getForObject(url,ResponseParse.class);
         System.out.println(response.getParse().getText());
 
         Document htmlResponse = Jsoup.parse(response.getParse().getText().get("*"));
+        Element table = htmlResponse.select("table").get(0);
+        Elements rows = table.select("tr");
+
+        Element directedBy = rows.get(2).select("td").get(0);
+
+
+        return directedBy.getElementsByTag("a").get(0).text();
+
+
 //        System.out.println(elements.toString());
-        return htmlResponse;
+//        return htmlResponse;
     }
 
 }
