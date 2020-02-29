c8cce36201d2cd0c700b7612ef3792f67a513adb
tejuamirthi <tejuamirthi@gmail.com>
2020-02-29 15:43:31 +0530

    api for getting all movies in the database with repository and service autowired in service and controller respectively

--- src/main/java/com/assignment/movielist/Services/MovieListServiceImplementation.java
+++ src/main/java/com/assignment/movielist/Services/MovieListServiceImplementation.java
@@ -1,11 +1,21 @@
 package com.assignment.movielist.Services;
 
+import com.assignment.movielist.Entities.Movie;
+import com.assignment.movielist.Repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieListServiceImplementation implements MovieListService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
     @Override
-    public String getListMovies() {
-        return null;
+    public List<Movie> getListMovies() {
+        return movieRepository.findAll();
     }
 }
