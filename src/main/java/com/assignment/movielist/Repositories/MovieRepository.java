e8a2ee010d300bb77e022f33cb68d3898893dd62
tejuamirthi <tejuamirthi@gmail.com>
2020-02-29 15:41:10 +0530

    Initial JPA repository with movie class

---
+++ src/main/java/com/assignment/movielist/Repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.assignment.movielist.Repositories;
+
+import com.assignment.movielist.Entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
--- src/main/java/com/assignment/movielist/Repositories/Repository.java
+++
@@ -1,6 +0,0 @@
-package com.assignment.movielist.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<> {
-}
