8003a2534c25218a69604c755d4f4de2bd9e1c23
tejuamirthi <tejuamirthi@gmail.com>
2020-03-06 22:08:30 +0530

    Dynamic query for filtering: initial commit

---
+++ src/main/java/com/assignment/movielist/Repositories/Custom/CustomMovieRepository.java
@@ -0,0 +1,12 @@
+package com.assignment.movielist.Repositories.Custom;
+
+import com.assignment.movielist.Entities.Movie;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CustomMovieRepository {
+
+    List<Movie> getMoviesByFilter(String name, Date releaseBefore, Date releaseAfter);
+
+}
