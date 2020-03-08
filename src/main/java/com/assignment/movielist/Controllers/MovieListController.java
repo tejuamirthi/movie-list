e4b6295d764ff3be591a8aba15557a092ec97658
tejuamirthi <tejuamirthi@gmail.com>
2020-03-08 10:55:56 +0530

    Made title as a request parameter and added released-after and before parameters

--- src/main/java/com/assignment/movielist/Controllers/MovieListController.java
+++ src/main/java/com/assignment/movielist/Controllers/MovieListController.java
@@ -9,14 +9,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.websocket.server.PathParam;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -46,9 +49,12 @@ public class MovieListController {
         return movieListService.getListMovies();
     }
 
-    @GetMapping(path = "/get-filter-list/{name}")
-    public List<MovieModel> getMoviesByFilter(@PathVariable("name") String name) {
-        return movieListService.getMoviesByFilter(name, null, null);
+    // TODO - change format for date
+    @GetMapping(path = "/get-filter-list")
+    public List<MovieModel> getMoviesByFilter(@RequestParam(value = "title", required = false) String name,
+                                              @RequestParam(value = "released-before", required = false) @DateTimeFormat(pattern = "dd/MM/yyyy") Date releasedBefore,
+                                              @RequestParam(value = "released-after", required = false) Date releasedAfter) {
+        return movieListService.getMoviesByFilter(name, releasedAfter, releasedBefore);
     }
 
 }
