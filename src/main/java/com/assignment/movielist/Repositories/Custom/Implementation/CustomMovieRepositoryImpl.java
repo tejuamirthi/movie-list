e3bf9b9b2e9878689f88ed20544996d1ad4af618
tejuamirthi <tejuamirthi@gmail.com>
2020-03-17 21:02:20 +0530

    Dynamic query done and removed commented code

--- src/main/java/com/assignment/movielist/Repositories/Custom/Implementation/CustomMovieRepositoryImpl.java
+++ src/main/java/com/assignment/movielist/Repositories/Custom/Implementation/CustomMovieRepositoryImpl.java
@@ -12,26 +12,9 @@ public class CustomMovieRepositoryImpl implements CustomMovieRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
-
-    // TODO complete the dynamic query
+    
     @Override
     public List<Movie> getMoviesByFilter(String name, Date releaseBefore, Date releaseAfter, String sortBy, boolean desc) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<User> query = cb.createQuery(User.class);
-//        Root<User> user = query.from(User.class);
-//
-//        Path<String> emailPath = user.get("email");
-//
-//        List<Predicate> predicates = new ArrayList<>();
-//        for (String email : emails) {
-//            predicates.add(cb.like(emailPath, email));
-//        }
-//        query.select(user)
-//                .where(cb.or(predicates.toArray(new Predicate[predicates.size()])));
-//
-//        return entityManager.createQuery(query)
-//                .getResultList();
-
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 
