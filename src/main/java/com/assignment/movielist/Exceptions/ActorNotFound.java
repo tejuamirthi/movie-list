384f31da5826ccd8f918ba2a250a5ffefe77a043
Tejeshwar Amirthy <tejuamirthi@gmail.com>
2022-03-25 16:00:27 +0530

    Revert "Actors crud"

--- src/main/java/com/assignment/movielist/Exceptions/ActorNotFound.java
+++ src/main/java/com/assignment/movielist/Exceptions/ActorNotFound.java
@@ -1,6 +1,5 @@
 package com.assignment.movielist.Exceptions;
 
-// new added comment
 public class ActorNotFound extends Exception {
     public ActorNotFound(String message) {
         super(message);
--- src/test/java/com/assignment/movielist/MovieListApplicationTests.java
+++ src/test/java/com/assignment/movielist/MovieListApplicationTests.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieListApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
+
 }
